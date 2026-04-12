32dc407269546584382a97f4d9a18ba09cc9ef30
Anicet <ton@email.com>
2026-04-12 08:49:35 -0400

    fix: hardcode Google Client ID en fallback

--- src/components/GoogleWrapper.tsx
+++ src/components/GoogleWrapper.tsx
@@ -1,14 +1,11 @@
 "use client";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
+const CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || "141147213471-sr4hs2m7467k3hfh2nhfr218760ck819.apps.googleusercontent.com";
+
 export default function GoogleWrapper({ children }: { children: React.ReactNode }) {
-  const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
-
-  // Si pas de Client ID configuré, on rend les enfants sans le provider
-  if (!clientId) return <>{children}</>;
-
   return (
-    <GoogleOAuthProvider clientId={clientId}>
+    <GoogleOAuthProvider clientId={CLIENT_ID}>
       {children}
     </GoogleOAuthProvider>
   );
